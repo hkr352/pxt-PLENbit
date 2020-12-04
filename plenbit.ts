1dfd8b7c03f5f2eeae5cc876b5e1403403cbd216
hkr352 <hkr352@gmail.com>
2020-12-04 15:07:55 +0900

    add setAngleToPosition Block

--- plenbit.ts
+++ plenbit.ts
@@ -137,7 +137,6 @@ namespace plenbit {
     let romAdr1 = 0x56;
     let initBle:boolean = false;
     let initPCA9865:boolean = false;
-    let plenDebug :boolean = false;
     let plenStrip: neopixel.Strip = null// update 11/26
     let plenEyeCreated = false;// update 11/26
     loadPos();
@@ -446,7 +445,31 @@ namespace plenbit {
 
     /**
      * Eight servos can be controlled at once
-     * @param angle 8 arrays
+     * @param angle 8 arrays : -90 ~ 90
+     * @param msec 100 ~ 1000
+     */
+    //% block="Set Angle -180.0 ~ 180.0|0:Left Shoulder : $ls|1:Left Thigh    : $lt|2:Left Arm      : $la|3:Left Foot     : $lf|4:Right Shoulder: $rs|5:Right Thigh   : $rt|6:Right Arm     : $ra|7:Right Foot    : $rf|msec %msec"
+    //% msec.min=100 msec.max=1000 msec.defl=500
+    //% ls.min=-90 ls.max=90 ls.defl=0
+    //% lt.min=-90 lt.max=90 lt.defl=0
+    //% la.min=-90 la.max=90 la.defl=0
+    //% lf.min=-90 lf.max=90 lf.defl=0
+    //% rs.min=-90 rs.max=90 rs.defl=0
+    //% rt.min=-90 rt.max=90 rt.defl=0
+    //% ra.min=-90 ra.max=90 ra.defl=0
+    //% rf.min=-90 rf.max=90 rf.defl=0
+    //% advanced=true
+    export function setAngleToPosition
+        (ls:number, lt:number, la:number, lf:number,
+         rs:number, rt:number, ra:number, rf:number, msec: number)
+    {
+        let angle = [ls*10, lt*10, la*10, lf*10,rs*10, rt*10, ra*10, rf*10];
+        setAngle(angle, msec);
+    }
+
+    /**
+     * Eight servos can be controlled at once
+     * @param angle 8 arrays : -1800 ~ 1800
      * @param msec 100 ~ 1000
      */
     //% block="Set Angle $angle, msec %msec"
@@ -454,16 +477,16 @@ namespace plenbit {
     //% advanced=true
     export function setAngle(angle: number[], msec: number) {
         let step = [0, 0, 0, 0, 0, 0, 0, 0];
-        msec = msec / motionSpeed;//now 20 //default 10; //speedy 30
+        msec = msec / motionSpeed;//now 20 //default 10 //speedy 30
         for (let val = 0; val < SERVO_NUM_USED; val++) {
             let target = (servoSetInit[val] - angle[val]);
             if (target != servoAngle[val]) {  // Target != Present
                 step[val] = (target - servoAngle[val]) / (msec);
             }
         }
-        for (let i = 0; i <= msec; i++)
-        {
+        for (let i = 0; i <= msec; i++) {
             let nowTime = input.runningTime();
+
             for (let val = 0; val < SERVO_NUM_USED; val++) {
                 servoAngle[val] += step[val];
                 servoWrite(val, (servoAngle[val] / 10));
